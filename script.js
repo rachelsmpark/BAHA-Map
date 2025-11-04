e7ccb6d25204610cb36536a90edd5e1fc2dfd9ff
rachelsmpark <156369192+rachelsmpark@users.noreply.github.com>
2025-11-03 18:13:37 -0800

    Increase map zoom level from 9 to 10

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
         container: 'map', // container ID
         style: 'mapbox://styles/rachelsmpark/cmh9ripoj00qm01smceizfkhr',
         center: [-122.27, 37.8], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-        zoom: 9 // starting zoom
+        zoom: 10 // starting zoom
     });
 
 map.on('load', function() {
